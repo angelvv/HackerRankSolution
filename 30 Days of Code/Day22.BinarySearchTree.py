1f028ef76f4891cb57026b63afe92d73fb8fdcf6
angelvv <angel.huang90@gmail.com>
2019-02-20 22:11:22 -0500

    add comments

--- 30 Days of Code/Day22.BinarySearchTree.py
+++ 30 Days of Code/Day22.BinarySearchTree.py
@@ -1,19 +1,19 @@
-class Node:
+class Node: # an object eg.house
     def __init__(self,data):
-        self.right=self.left=None
-        self.data = data
+        self.right=self.left=None # pointer = address eg.2 piece of blank paper in the house
+        self.data = data # data of this house, eg.n floors
 class Solution:
     def insert(self,root,data):
         if root==None:
-            return Node(data)
+            return Node(data) # create a house with n rooms, 2 pieces of blank paper to hold addresses 
         else:
-            if data<=root.data:
-                cur=self.insert(root.left,data)
-                root.left=cur
+            if data<=root.data: # if fewer rooms than this house
+                cur=self.insert(root.left,data) # find the empty address on the left paper to build the house
+                root.left=cur # write down this address on last houses paper
             else:
                 cur=self.insert(root.right,data)
                 root.right=cur
-        return root
+        return root # return this house
 
     def getHeight(self,root):
         #Write your code here
