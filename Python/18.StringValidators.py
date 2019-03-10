39ac12ca83d4d99318a90902f32b30eb558f27fc
angelvv <angel.huang90@gmail.com>
2019-03-10 10:21:42 -0400

    move to folder

--- Python/18.StringValidators.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    s = input()
-    
-    print(any(l.isalnum() for l in s))
-    print(any(l.isalpha() for l in s))
-    print(any(l.isdigit() for l in s))
-    print(any(l.islower() for l in s))
-    print(any(l.isupper() for l in s))
