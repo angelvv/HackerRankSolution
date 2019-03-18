072e10ee5badb466ba8da4c720525b84b1dcb76b
angelvv <angel.huang90@gmail.com>
2019-03-17 22:29:43 -0400

    add Python44

---
+++ Python/04.Math/44.IntegersComeInAllSizes.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+"""Nice Tutorial abt the memory management in Python 
+http://deeplearning.net/software/theano/tutorial/python-memory-management.html
+"""
+a,b,c,d = (int(input()) for _ in range(4))
+print(a**b + c**d)
