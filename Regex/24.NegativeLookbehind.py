39c7cc2a6d7ed96aa0a960b39ee27e9f9c9813fb
angelvv <angel.huang90@gmail.com>
2019-03-29 04:28:53 -0400

    add Regex24

---
+++ Regex/24.NegativeLookbehind.py
@@ -0,0 +1,13 @@
+"""
+Write a regex which can match all the occurences of characters which are not immediately preceded by vowels (a, e, i, u, o, A, E, I, O, U).
+"""
+
+Regex_Pattern = r"(?<![aeiouAEIOU])."	# Do not delete 'r'.
+
+import re
+
+Test_String = input()
+
+match = re.findall(Regex_Pattern, Test_String)
+
+print("Number of matches :", len(match))
